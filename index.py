c14d6397f9c0c3fb0d53ecfba99cfd2c205dfa99
Alex Alvarado <alexjalvarado66@gmail.com>
2022-10-02 14:18:54 -0400

    take video url as user input

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import os
 
-url = None #enter valid youtube video url here
+url = str(input("Enter url of the stream you want to download (live or archived), press enter to start download: \n"))
 
 command = str('yt-dlp.exe ' + url)
+
+print(command + "\n")
 
 os.system(command)
